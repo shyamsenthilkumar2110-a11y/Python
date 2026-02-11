6c339af994037f90c9ea1e86a657d03b405d9d8b
Shyam S <shyam.senthilkumar2110@gmail.com>
2026-02-11 23:12:03 +0530

    Add files via upload
    python code to check Adult or Mnior

---
+++ Adult_or_Minor.py
@@ -0,0 +1,6 @@
+user_name=str(input("Enter your user name : "))
+user_age=int(input("Enter your user age : "))
+if(user_age>=18):
+    print(f"Dear {user_name} congrigulation you are adult..")
+else:
+    print(f"Dear {user_name} sorry you are not adult..")
