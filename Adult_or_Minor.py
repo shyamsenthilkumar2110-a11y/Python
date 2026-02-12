9833da41e793257959142f09fb323e128a40bb09
Shyam <shyam.senthilkumar2110@gmail.com>
2026-02-12 23:39:57 +0530

    code changed

--- Adult_or_Minor.py
+++ Adult_or_Minor.py
@@ -4,3 +4,11 @@ if(user_age>=18):
     print(f"Dear {user_name} congrigulation you are adult..")
 else:
     print(f"Dear {user_name} sorry you are not adult..")
+
+# print number from 1 to age
+count=1
+user_array =[]
+while count<=user_age:
+     user_array.append(count)
+     count+=1
+print("the number from 1 to age is : ",user_array)
